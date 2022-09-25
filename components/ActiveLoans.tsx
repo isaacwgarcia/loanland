0aab40c33252a4d2f1c9580a5da1f4e619401f9a
Isaac Garcia <isaacwi@gmail.com>
2022-09-25 10:07:24 -0400

    loading progress

--- components/ActiveLoans.tsx
+++ components/ActiveLoans.tsx
@@ -1,29 +1,42 @@
 import React, { useEffect, useState, useContext } from "react";
 import { getActiveLoansbyBorrower } from "./lib/loan";
 import { AppContext } from "./state/context";
+import CircularProgress from "@mui/material/CircularProgress";
+import { Box } from "@mui/material";
 
 const ActiveLoans = () => {
   const [loans, setLoans] = useState([]);
   const [loaded, setLoaded] = useState(false);
+  const [loading, setLoading] = useState(false);
   const context = useContext(AppContext);
 
   useEffect(() => {
     (async () => {
+      setLoading(true);
       setLoaded(false);
       let loans = await getActiveLoansbyBorrower(context.state.session.address);
       if (loans.length > 0) {
         setLoaded(true);
+        setLoading(false);
         setLoans(loans);
         //console.log("Active borrowers loans: ", loans);
       }
     })();
   }, []);
 
-  if (loans.length == 0) return <>No active loans.</>;
-
   return (
     <div>
-      {loaded ? (
+      {loading ? (
+        <Box
+          display="flex"
+          justifyContent="center"
+          border="rounded"
+          height="80vh"
+          mt="10vh"
+        >
+          <CircularProgress color="inherit" />
+        </Box>
+      ) : loaded ? (
         <>
           {loans?.map((loan, index) => {
             return (
@@ -38,7 +51,7 @@ const ActiveLoans = () => {
           })}
         </>
       ) : (
-        <></>
+        <>No active loans.</>
       )}
     </div>
   );
--- components/LoansApplications.tsx
+++ components/LoansApplications.tsx
@@ -1,17 +1,22 @@
 import React, { useEffect, useState, useContext } from "react";
 import { getLoansbyBorrower } from "../components/lib/loan";
 import { AppContext } from "../components/state/context";
+import CircularProgress from "@mui/material/CircularProgress";
+import { Box } from "@mui/material";
 
 const LoansApplications = () => {
   const [loans, setLoans] = useState([]);
   const [loaded, setLoaded] = useState(false);
+  const [loading, setLoading] = useState(false);
   const context = useContext(AppContext);
 
   useEffect(() => {
     (async () => {
+      setLoading(true);
       setLoaded(false);
       let loans = await getLoansbyBorrower(context.state.session.address);
       if (loans.length > 0) {
+        setLoading(false);
         setLoaded(true);
         setLoans(loans);
         console.log("borrowers loans: ", loans);
@@ -20,7 +25,17 @@ const LoansApplications = () => {
   }, []);
   return (
     <div>
-      {loaded ? (
+      {loading ? (
+        <Box
+          display="flex"
+          justifyContent="center"
+          border="rounded"
+          height="80vh"
+          mt="10vh"
+        >
+          <CircularProgress color="inherit" />
+        </Box>
+      ) : loaded ? (
         <>
           {loans?.map((loan, index) => {
             return (
